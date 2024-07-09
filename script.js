37aa7b7739a8a42cc5c0cb987e2a81a2e462295f
David Khauan <thalsmatsumira@gmail.com>
2024-07-09 11:12:22 -0300

    projeto

---
+++ script.js
@@ -0,0 +1,36 @@
+let btnMenu = document.getElementById ('btn-menu')
+let menuMobile = document.getElementById ('menu-mobile')
+let overlay = document.getElementById ('overlay-menu')
+
+btnMenu.addEventListener ('click', () => {
+    menuMobile.classList.add ('abrir-menu')
+})
+
+menuMobile.addEventListener ('click', () => {
+    menuMobile.classList.remove ('abrir-menu')
+})
+
+overlay.addEventListener ('click', () => {
+    menuMobile.classList.remove ('abrir-menu')
+})
+
+window.onscroll = () => {
+    funcaoScroll()
+}
+
+function funcaoScroll() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById ('scroll').style.display = "block"
+    } else {
+        document.getElementById ('scroll').style.display = "none"
+    }
+}
+
+document.getElementById ('scroll').onclick = () => {
+    funcaoTopo()
+}
+
+function funcaoTopo() {
+    document.body.scrollTop = 0
+    document.documentElement.scrollTop = 0
+}
